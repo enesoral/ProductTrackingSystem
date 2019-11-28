a9ca5791f176a54770b18d1381aa0a689edea69a
enesoral <enesoral.seng@gmail.com>
2019-11-28 12:38:18 +0300

    db files and detail modal added.

--- ProductTrackingSystem/Controllers/ProductController.cs
+++ ProductTrackingSystem/Controllers/ProductController.cs
@@ -5,21 +5,28 @@ using System.Web;
 using System.Web.Mvc;
 using ProductTrackingSystem.Models;
 using PagedList;
+using ProductTrackingSystem.Models.Entity;
 
 namespace ProductTrackingSystem.Controllers
 {
     public class ProductController : Controller
     {
-        private SoftwareTrackingEntities db = new SoftwareTrackingEntities();
+        private ProductTrackingEntities db = new ProductTrackingEntities();
         public ActionResult Index(int page = 1, int pageSize = 4)
         {
             if (pageSize > 20)
                 pageSize = 20;
 
-            List<Product> products = db.Products.SqlQuery("SELECT * FROM PRODUCTS WHERE active = 1").ToList();
+            List<Product> products = db.Products.SqlQuery("SELECT * FROM PRODUCTS WHERE is_active = 1").ToList();
             PagedList<Product> model = new PagedList<Product>(products, page, pageSize);
 
             return View(model);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var model = db.Products.ToList().FirstOrDefault(x => x.id == id);
+            return PartialView("Detail", model);
         }
     }
 }
