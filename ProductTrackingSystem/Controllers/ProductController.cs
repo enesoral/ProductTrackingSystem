f20f1175530b33f77332ac1490bac75ca46b71dd
enesoral <enesoral.seng@gmail.com>
2019-12-15 13:20:31 +0300

    methods added to productcontroller for add product

--- ProductTrackingSystem/Controllers/ProductController.cs
+++ ProductTrackingSystem/Controllers/ProductController.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Globalization;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.UI;
@@ -12,14 +16,10 @@ namespace ProductTrackingSystem.Controllers
 {
     public class ProductController : Controller
     {
-        private ProductTrackingEntities db = new ProductTrackingEntities();
-        public ActionResult Index(string searching, int ddlListType = 1, int ddlMonth = 1, int page = 1, int pageSize = 4)
+        private readonly ProductTrackingEntities1 db = new ProductTrackingEntities1();
+        public ActionResult Index(string searching, int? ddlListType, int ddlMonth = 1, int page = 1)
         {
-
-            if (pageSize > 20)
-                pageSize = 20;
-
-            List<Product> products = null;
+            List<Product> products;
 
             switch (ddlListType)
             {
@@ -27,19 +27,19 @@ namespace ProductTrackingSystem.Controllers
                     products = new List<Product>(db.Products.Where(p => !p.is_active));
                     break;
                 case 3:
-                    products = new List<Product>(db.Products.SqlQuery(
+                    products = new List<Product>((IEnumerable<Product>) db.Products.SqlQuery(
                         "SELECT * FROM Products WHERE DATEDIFF(day, GETDATE(), expiration_date) <= {0} AND DATEDIFF(day, GETDATE(), expiration_date) >= 0", ddlMonth * 30));
                     break;
                 case 4:
-                    products = new List<Product>(db.Products.SqlQuery(
+                    products = new List<Product>((IEnumerable<Product>) db.Products.SqlQuery(
                         "SELECT * FROM Products WHERE DATEDIFF(day, GETDATE(), expiration_date) <= 0"));
                     break;
                 case 5:
-                    products = new List<Product>(db.Products.SqlQuery(
+                    products = new List<Product>((IEnumerable<Product>) db.Products.SqlQuery(
                         "SELECT * FROM Products WHERE MONTH(renovation_date) <= 6 AND YEAR(renovation_date) = YEAR(GETDATE())"));
                     break;
                 case 6:
-                    products = new List<Product>(db.Products.SqlQuery(
+                    products = new List<Product>((IEnumerable<Product>) db.Products.SqlQuery(
                         "SELECT * FROM Products WHERE MONTH(renovation_date) > 6 AND YEAR(renovation_date) = YEAR(GETDATE())"));
                     break;
                 case 7:
@@ -57,7 +57,7 @@ namespace ProductTrackingSystem.Controllers
                 if (!searching.Trim().Equals(""))
                     products = new List<Product>(db.Products.Where(x => x.name.Contains(searching.Trim()) || x.specification_name.Contains(searching.Trim())));
 
-            PagedList<Product> model = new PagedList<Product>(products.ToList(), page, pageSize);
+            PagedList<Product> model = new PagedList<Product>(products.ToList(), page, 1);
 
             return View(model);
         }
@@ -66,6 +66,67 @@ namespace ProductTrackingSystem.Controllers
         {
             var model = db.Products.ToList().FirstOrDefault(x => x.id == id);
             return PartialView("Detail", model);
+        }
+
+        [HttpGet]
+        public ActionResult AddProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddProduct(FormCollection form)
+        {
+            Product product = new Product();
+            
+            product.name = form["txtName"].Trim();
+            product.specification_name = form["txtSpecificationName"].Trim();
+            product.renovation_date = formatToDate(form["txtPurchaseDate"]);
+            string expiration_date = product.renovation_date.ToString();
+
+            if (form["chkLimitless"].Equals("false"))
+            {
+                if (form["ddlTermType"].Equals("1"))
+                {
+                    int addDays = Convert.ToInt32(form["txtNumber"]) * 365;
+                    product.expiration_date = Convert.ToDateTime(expiration_date).AddDays(addDays);
+                }
+                else
+                {
+                    int addDays = Convert.ToInt32(form["txtNumber"]) * 30;
+                    product.expiration_date = Convert.ToDateTime(expiration_date).AddDays(addDays);
+                }
+            }
+            else
+            {
+                product.expiration_date = Convert.ToDateTime("9999-12-12");
+            }
+
+            
+            product.license_fee = form["txtLicenseFee"].Trim();
+            product.currency_type = form["ddlCurrencyType"].Equals("1") ? "₺(TL)" : "$(USD)";
+            product.license_type = form["txtLicenseType"].Trim();
+            product.usage_type = form["ddlUsageType"].Equals("1");
+            product.payer = form["txtPayer"].Trim();
+            product.seller = form["txtSeller"].Trim();
+            product.owner = form["txtOwner"].Trim();
+            product.req_faculty = form["txtReqFaculty"].Trim();
+            product.req_personnel = form["txtReqPersonnel"].Trim();
+            product.users = form["txtUsers"].Trim();
+            product.is_active = form["ddlIsActive"].Equals("1");
+            product.explanation = form["txtExplanation"].Trim();
+
+            db.Products.Add(product);
+            db.SaveChanges();
+
+            return RedirectToAction("Index", "Product");
+        }
+
+        private DateTime formatToDate(string strDate)
+        {
+            string[] arr = strDate.Split('-');
+            string date = arr[0] + "/" + arr[1] + "/" + arr[2];
+            return DateTime.ParseExact(date, "dd/MM/yyyy", null);
         }
     }
 }
